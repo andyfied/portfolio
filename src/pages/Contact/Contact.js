19e5a44396753dfc1ec796f077c6fc69af5f1724
Viktor Ahlund <viktor.ahlund@gmail.com>
2018-06-09 01:58:12 +0200

    Started working on Contact

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -2,8 +2,14 @@ import React from 'react'
 import './Contact.css'
 
 const Contact = () => (
-    <div>
+    <div id="page-container">
         <h1>Kontakt</h1>
+
+        <div id="bytherese-main-container">
+            <div id="bytherese-title-container">
+                <p id="bytherese-title">From Norrland. By Therese.</p>
+            </div>
+        </div>
     </div>
 )
 
