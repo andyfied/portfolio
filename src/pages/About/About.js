3807d4eaf4e1fc72e5e0f2c2464b6bc73db71d3d
Viktor Åhlund <viktorahlund@Viktors-MacBook-Pro.local>
2019-06-15 12:57:43 +0200

    Started implementing the desktop app

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './About.css'
 import bgImage from './mobile/assets/background.jpg'
+import MediaQuery from 'react-responsive'
+import breakpoints from '../../constants/breakpoints'
 
 const styles = {
   article: {
@@ -14,26 +16,60 @@ const styles = {
 
 const About = () => (
   <div className="pageContent">
-    <img style={styles.img} alt="About" src={bgImage} />
-    <article style={styles.article}>
-      <h2>About</h2>
-      <p>
-        For as long as I can remember, I have felt the need to create, to express myself through art. Being a creative
-        mind, I love to capture moments, turn feelings into colours and share it with you through my paintings.
-      </p>
-      <p>
-        Today, I spend most of my time drawing in Stavalund, a family farm located deep into the northern Swedish woods.
-        Surrounded by the nature and it’s stillness, this is where I find my peace and inspiration.
-      </p>
-      <p>
-        All of my paintings hold a foundation of nature, strength, calm and freedom. Those elements make the very core
-        of my drawing – the wild and the free. With my portraits, I hope to mirror those souls and individuals so
-        important to you and others they need to be forever kept in colour.
-      </p>
-      <p>
-        <i>/Therese Åhlund</i>
-      </p>
-    </article>
+    <MediaQuery maxWidth={breakpoints.MOBILE_BREAKPOINT}>
+      <img style={styles.img} alt="About" src={bgImage} />
+      <article style={styles.article}>
+        <h2>About</h2>
+        <p className="font-about">
+          For as long as I can remember, I have felt the need to create, to express myself through art. Being a creative
+          mind, I love to capture moments, turn feelings into colours and share it with you through my paintings.
+        </p>
+        <p className="font-about">
+          Today, I spend most of my time drawing in Stavalund, a family farm located deep into the northern Swedish
+          woods. Surrounded by the nature and it’s stillness, this is where I find my peace and inspiration.
+        </p>
+        <p className="font-about">
+          All of my paintings hold a foundation of nature, strength, calm and freedom. Those elements make the very core
+          of my drawing – the wild and the free. With my portraits, I hope to mirror those souls and individuals so
+          important to you and others they need to be forever kept in colour.
+        </p>
+        <p className="font-about">
+          <i>/Therese Åhlund</i>
+        </p>
+      </article>
+    </MediaQuery>
+    <MediaQuery minWidth={breakpoints.DESKTOP_BREAKPOINT}>
+      <div className="row">
+        <div className="col">
+          <div className="leftside">
+            <h2>About</h2>
+            <p className="font-about">
+              For as long as I can remember, I have felt the need to create, to express myself through art. Being a
+              creative mind, I love to capture moments, turn feelings into colours and share it with you through my
+              paintings.
+            </p>
+            <p className="font-about">
+              Today, I spend most of my time drawing in Stavalund, a family farm located deep into the northern Swedish
+              woods. Surrounded by the nature and it’s stillness, this is where I find my peace and inspiration.
+            </p>
+            <p className="font-about">
+              All of my paintings hold a foundation of nature, strength, calm and freedom. Those elements make the very
+              core of my drawing – the wild and the free. With my portraits, I hope to mirror those souls and
+              individuals so important to you and others they need to be forever kept in colour.
+            </p>
+            <p className="font-about">
+              <i>/Therese Åhlund</i>
+            </p>
+          </div>
+        </div>
+        <div className="col">
+          <div className="rightside" />
+        </div>
+        <div className="col">
+          <div className="bottomright" />
+        </div>
+      </div>
+    </MediaQuery>
   </div>
 )
 
--- src/pages/Pricing/Pricing.js
+++ src/pages/Pricing/Pricing.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Pricing.css'
 import bgImage from './header-img.jpg'
+import MediaQuery from 'react-responsive'
+import breakpoints from '../../constants/breakpoints'
 
 const styles = {
   pageContent: {
@@ -21,32 +23,62 @@ const styles = {
 
 const Pricing = () => (
   <div style={styles.pageContent}>
-    <img className="header-img" alt="About" src={bgImage} />
-    <h2>Customized Paintings</h2>
-    <section>
-      <p style={styles.section}>
-        Prices may vary due to size, theme etc. To get an exact price for your customized painting, please send me some
-        information about the order you want to place.
-      </p>
-      <section className="card-view">
-        <h3>40 x 50 cm</h3>
-        <h4>
-          <em>From 3999 SEK</em>
-        </h4>
-        <p>
-          <em>Includes framing</em>
+    <MediaQuery maxWidth={breakpoints.MOBILE_BREAKPOINT}>
+      <img className="header-img" alt="About" src={bgImage} />
+      <h2>Customized Paintings</h2>
+      <section>
+        <p style={styles.section}>
+          Prices may vary due to size, theme etc. To get an exact price for your customized painting, please send me
+          some information about the order you want to place.
         </p>
+        <section className="card-view">
+          <h3>40 x 50 cm</h3>
+          <h4>
+            <em>From 3999 SEK</em>
+          </h4>
+          <p>
+            <em>Includes framing</em>
+          </p>
+        </section>
+        <section className="card-view">
+          <h3>50 x 70 cm</h3>
+          <h4>
+            <em>From 6499 SEK</em>
+          </h4>
+          <p>
+            <em>Includes framing</em>
+          </p>
+        </section>
       </section>
-      <section className="card-view">
-        <h3>50 x 70 cm</h3>
-        <h4>
-          <em>From 6499 SEK</em>
-        </h4>
-        <p>
-          <em>Includes framing</em>
-        </p>
-      </section>
-    </section>
+    </MediaQuery>
+    <MediaQuery minWidth={breakpoints.DESKTOP_BREAKPOINT}>
+      <h2>Customized Paintings</h2>
+      <div className="row">
+        <div className="col">
+          <img className="header-img-desktop" alt="About" src={bgImage} />
+          <div className="rightside-desktop">
+            <div className="card-view-desktop">
+              <h3>40 x 50 cm</h3>
+              <h4>
+                <em>From 3999 SEK</em>
+              </h4>
+              <p>
+                <em>Includes framing</em>
+              </p>
+            </div>
+            <div className="card-view-desktop">
+              <h3>50 x 70 cm</h3>
+              <h4>
+                <em>From 6499 SEK</em>
+              </h4>
+              <p>
+                <em>Includes framing</em>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </MediaQuery>
   </div>
 )
 
